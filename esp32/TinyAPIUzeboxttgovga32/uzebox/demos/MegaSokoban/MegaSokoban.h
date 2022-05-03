0a1077c7a45706914e52d7050b9bc70a3765b088
ackerman <jaime_jj@hotmail.com>
2022-05-03 11:56:23 +0200

    Megasokoban demo

---
+++ esp32/TinyAPIUzeboxttgovga32/uzebox/demos/MegaSokoban/MegaSokoban.h
@@ -0,0 +1,15 @@
+#ifndef MEGASOKOBAN_H_
+#define MEGASOKOBAN_H_
+
+#include "../../gbConfig.h" 
+
+#ifdef use_rom_megasokoban
+ void do_megasokoban_Setup(void);
+ void do_megasokoban_main(void);
+#endif
+
+#endif
+
+
+
+
