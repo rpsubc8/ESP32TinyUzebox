3015aa21b8961441f2808f92f049633e7b54a386
ackerman <jaime_jj@hotmail.com>
2022-05-03 11:55:56 +0200

    ControllerTester demo

---
+++ esp32/TinyAPIUzeboxttgovga32/uzebox/demos/ControllerTester/ControllerTester.h
@@ -0,0 +1,13 @@
+#ifndef _GB_CONTROLLERTESTER_H
+ #define _GB_CONTROLLERTESTER_H
+ #include "../../gbConfig.h"
+ #include "../../gbGlobals.h"
+ 
+ //No existia
+ 
+ #ifdef use_rom_controllertester
+  void do_ControllerTester_Setup(void);
+  void do_ControllerTester_main(void);
+ #endif 
+
+#endif
